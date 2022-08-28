34f4c05a48c477dbc19e96f3cd5d90cdde548853
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-08-28 19:03:18 +0300

    improved apHeader style

--- layouts/partials/app-header.vue
+++ layouts/partials/app-header.vue
@@ -13,20 +13,21 @@ export default {
 <style lang="scss" scoped>
 .app-header {
   width: 100%;
-  height: 60px;
+  height: 50px;
   background-color: $gray-two;
   position: sticky;
   top: 0;
+  border-bottom: 1px solid $dark-three;
   &__logo {
-    width: 80px;
-    height: 80px;
+    width: 70px;
+    height: 70px;
     border-radius: 50%;
     background-color: white;
-    margin-top: 30px;
-    border: 7px solid $dark-three;
+    margin-top: 40px;
+    border: 3px solid $dark-three;
     box-shadow: rgba(0, 0, 0, 0.221) 0px 7px 8px;
     img {
-      width: 50px;
+      width: 40px;
     }
   }
 }
