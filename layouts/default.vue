6632433396056bbf919aeb04e6086c4024528999
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-08-28 19:00:07 +0300

    added app-footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,17 @@
   <div class="default-layout flex--column column--middle--center">
     <app-header />
     <router-view />
+    <app-footer />
   </div>
 </template>
 <script>
 import AppHeader from './partials/app-header.vue'
+import AppFooter from './partials/app-footer.vue'
 export default {
   name: 'DefaultLayout',
   components: {
     AppHeader,
+    AppFooter,
   },
 }
 </script>
---
+++ layouts/partials/app-footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="app-footer flex--column column--middle--center">
+    <h5>
+      Bu site <strong>Ahmet İlhan</strong> tarafından kodlanmış olup,
+      <i>Tüm alkol severlere armağan edilmiştir.</i>
+    </h5>
+    <br />
+    <p><i>"Ne kadar içersen iç, başın döner. Gidenler değil!"</i></p>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'AppFooter',
+}
+</script>
+<style lang="scss" scoped>
+.app-footer {
+  width: 100%;
+  padding: 20px;
+  background-color: $gray-three;
+  margin-top: 40px;
+  h5 {
+    @include golden-text(16);
+  }
+  p {
+    @include golden-text(14);
+    opacity: 0.7;
+  }
+}
+</style>
