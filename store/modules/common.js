42caf9f045edca5fe9f910fb1a5d1da4cd6e4106
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-08-29 23:30:56 +0300

    removed consoles

--- store/modules/common.js
+++ store/modules/common.js
@@ -5,7 +5,6 @@ export default {
   }),
   mutations: {
     setSearchModalVisible(state, payload) {
-      console.log(payload)
       state.isSearchModalVisible = payload
     },
     setSearchQuery(state, payload) {
